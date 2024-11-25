7866f98db2117e3a4772e9769a705a529ebcf187
dashcaj <158211825+dashcaj@users.noreply.github.com>
2024-11-25 13:59:33 -0500

    s

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,34 @@
-import "./css/style.css"
+import "../css/style.css";
 
 const api = "https://foodish-api.com/"
 
 async function getData(api){
-    const response = await api
-    try ()
-}
+    try {
+    const response = fetch(api);
+    const data = await response.json();
+    document.getElementById("api-response").textContent = data.content;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+getData(api);
+
+
+
+
+
+
+
+
+
+
+
+
+/* function killDustin(api){
+    try {
+        const knife = fetch(api);
+        const gun = await response.json(trigger);
+
+    }
+} */
