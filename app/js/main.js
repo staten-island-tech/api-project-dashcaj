1074540bacab66b9e4e283aff93765989f7f1900
dashcaj <158211825+dashcaj@users.noreply.github.com>
2024-11-26 13:59:55 -0500

    p[k

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,12 @@
 import "../css/style.css";
 
-const api = "https://foodish-api.com/"
 
-async function getData(api){
-    try {
-    const response = fetch(api);
-    const data = await response.json();
-    document.getElementById("api-response").textContent = data.content;
-    } catch (error) {
-        console.log(error);
-    }
+
+async function getData(){
+    const api = await fetch(
+        "https://api.imgflip.com/get_memes"
+    );
+    
 };
 
 getData(api);
