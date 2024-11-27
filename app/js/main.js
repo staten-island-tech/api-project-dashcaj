70a633076dfe312bfe111b52f3dc54c6a1db9d14
dashcaj <158211825+dashcaj@users.noreply.github.com>
2024-11-27 13:04:31 -0500

    ko

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,25 @@
 import "../css/style.css";
 
+const DOMSelectors = {
+
+};
+
+function createCard() {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.innerHTML = `
+    <div class="box">
+            <h2>${name}</h2>
+            <p>${position}</p>
+            <img class="pictures1" src="${image}" alt="${name}">
+            <div>
+            <button class="deletebtn">Delete</button>
+            </div>
+    </div>
+    `;
+};
+
+
 
 
 async function getData(){
@@ -19,13 +39,3 @@ getData(api);
 
 
 
-
-
-
-/* function killDustin(api){
-    try {
-        const knife = fetch(api);
-        const gun = await response.json(trigger);
-
-    }
-} */
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,7 +1,6 @@
-import('tailwindcss').Config
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./main.js", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./js/main.js", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
