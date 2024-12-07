7c4c82c23283ba9ee8968df499cbfa005d51c5d0
dashcaj <dustinc7@nycstudents.net>
2024-12-07 13:02:22 -0500

    jioojjo

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,7 @@ import "../css/style.css";
 
 const url = "https://www.amiiboapi.com/api/amiibo/"; 
 const searchbox = document.getElementById("searchbar");
+const butt = document.getElementById("butt");
 
 
 async function getData(searchTerm = '') {
@@ -18,20 +19,20 @@ async function getData(searchTerm = '') {
 
 
 function cardBox(data) {
-    const box = document.getElementById("container");
+    const box = document.querySelector(".container");
     box.innerHTML = "";
     
     if (!data || !data.amiibo || data.amiibo.length === 0) {
-        box.innerHTML = "<p>No Amiibo found matching your search.</p>";
+        box.innerHTML = "<p>there are no amiibo</p>";
         return;
     }
 
     data['amiibo'].forEach((amiibo) => {
         const html = `
-            <div class="card">
+            <div class="card w-60 rounded-[14px] border-8 font-semibold border-white m-4 bg-slate-300">
                 <h1>${amiibo.gameSeries}</h1>
                 <h2>${amiibo.name}</h2>
-                <img class="pictures1" src="${amiibo.image}" alt="${amiibo.character}">
+                <img class="pictures1 w-3/5 h-3/6 my-8" src="${amiibo.image}" alt="${amiibo.character}">
                 <h4>${amiibo.character}</h4>
             </div>
         `;
@@ -39,7 +40,8 @@ function cardBox(data) {
     });
 }
 
-searchbox.addEventListener("input", () => {
+butt.addEventListener("click", (event) => {
+    event.preventDefault()
     const usersearch = searchbox.value.toLowerCase().trim();
     getData(usersearch); 
 });
