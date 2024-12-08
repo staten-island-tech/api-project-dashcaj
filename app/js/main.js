6c74343c2629b72ac12d704793837b36a31ccdc6
dashcaj <dustinc7@nycstudents.net>
2024-12-07 21:38:41 -0500

    wqedfaefefaw

--- app/js/main.js
+++ app/js/main.js
@@ -29,10 +29,10 @@ function cardBox(data) {
 
     data['amiibo'].forEach((amiibo) => {
         const html = `
-            <div class="card w-60 rounded-[14px] border-8 font-semibold border-white m-4 bg-slate-300">
+            <div class="card w-60 rounded-[14px] border-8 font-semibold border-[rgb(187,159,0)] m-4 bg-slate-300 bg-black">
                 <h1>${amiibo.gameSeries}</h1>
                 <h2>${amiibo.name}</h2>
-                <img class="pictures1 w-3/5 h-3/6 my-8" src="${amiibo.image}" alt="${amiibo.character}">
+                <img class="pictures1 w-3/5 h-3/6 mx-auto my-8" src="${amiibo.image}" alt="${amiibo.character}">
                 <h4>${amiibo.character}</h4>
             </div>
         `;
